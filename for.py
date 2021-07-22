32125c6a3303de743a5108d6dcc46639331ca2f9
Anila Sree <anilasrees@gmail.com>
2021-07-22 21:40:06 +0530

    added for.py

---
+++ for.py
@@ -0,0 +1,44 @@
+from typing import Set
+
+
+a = 'hellow world'
+for i in a:
+    print(i)
+days = ["monday","tuesday","wednesday"]  
+for day in days:
+    print(day)
+num = ["one","two","three","four"]
+for no in num:
+    print(no) 
+for i in range(1,6):
+    print(i)                       
+for a in range(5,10):
+    print(a)
+#dictionary
+dict = {
+    "name": "ani",
+    "age": 34,
+        "place": "tkm",
+        "phn": 9234780011 
+}
+#del dict
+print(dict)
+a = dict["name"]
+print(a)
+g = dict["age"]
+print(g)
+dict2 = {
+    "item" : "mangos",
+    "number" : 34,
+    "special" : "malgoa",
+    "code" : 345
+}
+print(dict2)
+z = dict2["number"]
+print(z)
+a = 6
+if a > 0:
+    print("it is a positive number")
+elif a > 8:
+    print("it is a negative number")
+        
