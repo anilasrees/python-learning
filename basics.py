135e6d1f16cd8cda95807da8324c62ed323a867c
Anila Sree <anilasrees@gmail.com>
2021-07-26 17:04:29 +0530

    palindrome

--- basics.py
+++ basics.py
@@ -1,5 +1,28 @@
+"""
+#input values and find sum
+
 valu1 = int(input("enter the first dgit: "))
 valu2 = int(input("enter the second value : "))
-sum = value1 + valu2
-print("you sum is : "+sum)
+sum = valu1 + valu2
+print("sum of these two numbers is : "+str(sum))
 
+
+#if_elif_else
+
+i = int(input("enter an integer :"))
+if i ==0:
+    print("it is a zero")
+elif i >=1:
+    print("it is a positive number")
+elif i <=-1:
+    print("it is a negative number")
+    """
+
+#palindrome 
+
+string = input("enter a word : ")
+rev_string = string [::-1]
+if string == rev_string :
+    print("this word is a palindrome.")
+else:
+    print("this is not a palindrome.")
