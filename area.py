1a5f22aec503b50784dc25afd5183d64ad294098
Anila Sree <anilasrees@gmail.com>
2021-08-02 20:47:08 +0530

    heron's using function

--- area.py
+++ area.py
@@ -1,6 +1,12 @@
-a = float(input("enter first side of triangle"))
-b = float(input("enter the second side of triangle"))
-c = float(input("enter the third side of triangle"))
-s = (a+b+c)/2
-area = (s*(s-a)*(s-b)*(s-c))**.5
-print ("the area = " + str(area))
+def heronsformula(a,b,c):
+    s = (a+b+c)/2
+    area = (s*(s-a)*(s-b)*(s-c))**0.5
+    return area
+
+side1 = float(input("enter the first side of the triangle  "))
+side2 = float(input("enter the second side of the triangle "))
+side3 = float(input("enter the third side of the triangle  "))
+
+area = heronsformula(side1,side2,side3)
+
+print("area of the triangle using heron's formula is = " + str(area))
