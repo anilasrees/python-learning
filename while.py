72a845dca3920c55d573ed15424ba4a80f54ecb7
Anila Sree <anilasrees@gmail.com>
2021-08-06 19:13:31 +0530

    sum from one digit to another digit

--- while.py
+++ while.py
@@ -1,5 +1,8 @@
-#print 20 asterisks
-i = 1
-while(i<21):
-    print("*",end= '\t')
-    i+=1
+# sum of numbers from m to n
+m = int(input("enter the value of starting digit : "))
+n = int(input("enter the value of ending digit : "))
+s = 0
+while(m<=n):
+    s = s+m
+    m=+1
+print("sum = "+s)
