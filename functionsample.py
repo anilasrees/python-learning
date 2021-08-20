32335dabb98e9c3a0ab40f0d3b616fd181472866
Anila Sree <anilasrees@gmail.com>
2021-08-20 09:56:04 +0530

    fuction_sum

--- functionsample.py
+++ functionsample.py
@@ -1,11 +1,5 @@
-def function1(name,age):
-    print("my name is "+name +" age is "+str(age))
-    print("*******************")
-def function2(house,place):
-    print("my house name is "+  house  +" i am from "+place)
-name = "nila"
-house = "kariyil"
-place = "tkm"
-age = 20
-function1("malu",22)
-function2(house,place)
+def add_num(a,b):
+    sum = a+b
+    return sum
+valu = add_num(10,23)
+print (valu)
