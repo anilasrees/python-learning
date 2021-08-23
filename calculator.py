4b24e40f9dd227b3922aa0b22b6048953d67a37b
Anila Sree <anilasrees@gmail.com>
2021-08-24 05:12:11 +0530

    function changes happen

--- calculator.py
+++ calculator.py
@@ -2,36 +2,46 @@ import math
 print("\t  CALCULATOR   ")
 def sum(a,b):
     a+=b
-    return a
+    
+    print ("The sum of these numbers is : "+str(a))
 
 def sub(a,b):
     if a>b:
         a-=b
-        return a
+        
+        print("\nThe difference is : "+str(a))
     else:
         b-=a
-        return b
+        
+        print("\nThe difference is : "+str(b))
 def mul(a,b):
     a*=b
-    return a
+    
+    print("\nThe product is : "+str(a))
 def div(a,b):
     q=a/b
     r=a%b
     print(" the quotient is : "+str(q))
     print("  the remainder is :"+str(r))
-def sqr(a):
+def sqrt(a):
     x=math.sqrt(a)
-    return x
+    
+    print("\nthe squre root is : "+str(x))
+
+def sqr(a):
+    t=a*a
+    print("The square is : "+str(t))
 
 
 while(True):
-    print(" choose the operation you want to perform : ")
+    print("\tchoose the operation you want to perform : ")
     print("\t1.ADDITION")
     print("\t2.SUBTRACTION")
     print("\t3.MULTIPLICATION")
     print("\t4.DIVISION")
     print("\t5.SQUARE ROOT")
-    print("\t6.EXIT")
+    print("\t6.SQUARE")
+    print("\t7.EXIT")
     
     choice = int(input('>'))
     
@@ -39,27 +49,30 @@ while(True):
         print("Enter two numbers : ")
         num1 = int(input('>'))
         num2 = int(input('>'))
-        s=sum(num1,num2)
-        print ("The sum of these numbers is : "+str(s))
+        sum(num1,num2)
+        
     elif choice==2:
         print("\n\nEnter two numbers : ")
         num1=int(input('>'))
         num2=int(input('>'))
-        b = sub(num1,num2)
-        print("\nThe difference is : "+str(b))
+        sub(num1,num2)
+       
     elif choice==3:
         num1=int(input("Enter the first number"))
         num2=int(input("Enter the second number"))
-        p=mul(num1,num2)
-        print("\nThe product is : "+str(p))
+        mul(num1,num2)
+       
     elif choice==4:
         num1=int(input("Enter the first digits"))
         num2=int(input("Enter the second number"))
         div(num1,num2)
     elif choice==5:
         num1=int(input("Enter the number"))
-        r=sqrt(num1)
-        print("\nthe squre root is : "+str(r))
+        sqrt(num1)
+     
+    elif choice==6:
+        num1=int(input("Enter the value : "))
+        sqr(num1)
     else:
-        print("you choose to exit...")
+        print("you choose to exit...thank you...")
         break
