50c24ac23febf06ed6fd23e9d79fbbe9e26ee60c
anilasrees <87823961+anilasrees@users.noreply.github.com>
2021-09-05 00:07:32 +0530

    Update area.py
    function name changed

--- area.py
+++ area.py
@@ -1,8 +1,8 @@
-def area_circle(radius):
+def area_circle1(radius):
     radius = float(input("enter the radius of the circle : "))
     area = 3.14*radius*radius
     circumference = 2*3.14*radius
     print("AREA = "+str(round(area,2)))
     print("CIRCUMFERENCE = "+str(round(circumference,3)))
 
-area_circle(1)
+area_circle1(1)
